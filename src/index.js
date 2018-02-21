35011b040e70c3e3f8b067ff5b45dd2c732fb2f7
William Dunlop <william.dunlop.1993@gmail.com>
2018-02-21 21:22:10 +1100

    removed pointless console log

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import './style/css/index.css';
 import App from './components/app';
 
-console.log('sup from index.js');
-
 ReactDOM.render(<App />, document.getElementById('root'));
